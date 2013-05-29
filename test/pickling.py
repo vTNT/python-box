30ef505207ec1858ca6447dfcf608594f47cac2c
linyd <linyd@server.domain.com>
2013-05-29 17:10:39 +0800

    add the end scrips

---
+++ test/pickling.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import cPickle as p
+#import pickle as p
+
+shoplistfile = 'shoplist.data'
+print shoplistfile
+# the name of the file where we will store the object
+
+shoplist = ['apple', 'mango', 'carrot']
+
+# Write to the file
+f = file(shoplistfile, 'w')
+p.dump(shoplist, f) # dump the object to a file
+f.close()
+
+del shoplist # remove the shoplist
+
+# Read back from the storage
+f = file(shoplistfile)
+storedlist = p.load(f)
+print storedlist
