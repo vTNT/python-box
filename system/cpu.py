eda1d6f732a32d27b701925edf861f6d60df18e3
linyd <vbb625@gmail.com>
2013-05-30 15:23:28 +0800

    add cpu python scripts

---
+++ system/cpu.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def cpu_stat():
+    cpu = []
+    i = 0
+    cpuinfo = {}
+    f = open("/proc/cpuinfo")
+    lines = f.readlines()
+    f.close()
+    for line in lines:
+        if line == '\n':
+            cpu.append('cpuinfo')
+            i += 1
+        if len(line) < 2:
+            continue
+        name = line.split(':')[0].rstrip()
+        var = line.split(':')[1]
+        cpuinfo[name] = var
+    print cpuinfo
+    print cpu
+    return i,cpu
+if __name__ == '__main__':
+    c =cpu_stat()
+    print "the core number is %s " % c[0]
+    #print c
+
+
+
