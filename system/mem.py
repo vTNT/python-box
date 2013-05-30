572d8a72c9e3f85a2ee894eb6d3e8dc106d57a2b
linyd <linyd@server.domain.com>
2013-05-30 13:39:08 +0800

    add memory

--- system/mem.py
+++ system/mem.py
@@ -5,13 +5,22 @@ def memory_stat():
     mem = {}
     f = open("/proc/meminfo")
     lines = f.readlines()
-    print lines
     f.close()
-    
     for line in lines:
-        if len(line) < 2: continue
+        if len(line) < 2:
+            continue
         name = line.split(':')[0]
         var = line.split(':')[1].split()[0]
-        mem[name] = long(var) * 1024.0
-    mem['Memused'] = mem['MemTotal'] - mem['MemFree'] - mem['Buffers'] - mem['cached']
+        mem[name] = int(long(var) / 1024.0)
+    #print mem['MemTotal']
+    mem['Memused'] = mem['MemTotal'] - mem['MemFree'] - mem['Buffers'] - mem['Cached']
     return mem
+
+if __name__ == '__main__':
+   mem = memory_stat()
+   #print mem
+   print "the total is %s Mb" % mem['MemTotal']
+   print "the free is %s Mb" % mem['MemFree']
+   print "the used is %s Mb" % mem['Memused']
+
+
