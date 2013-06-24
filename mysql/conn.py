7ae6384e61fab4a11a24eb4cb3b8e0e4038b2c13
linyd <vbb625@gmail.com>
2013-06-24 14:56:35 +0800

    add mysql.py

---
+++ mysql/conn.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import MySQLdb
+def getdata():
+    try:
+        conn = MySQLdb.connect(host='localhost',user='root',passwd='ename110',port=3306)
+        cur = conn.cursor()
+        
+        cur.execute('select user,host from mysql.user')
+        cur.close()
+        conn.close()
+    except MySQLdb.Error,e:
+        print "Mysql Error %d: %s" % (e.args[0],e.args[1])
+
+if __name__ == '__main__':
+    getdata()
---
+++ mysql/fetch.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import MySQLdb
+
+try:
+    conn=MySQLdb.connect(host='localhost',user='root',passwd='ename110',port=3306)
+    cur = conn.cursor()
+
+    conn.select_db('python')
+
+    count = cur.execute('select * from test')
+    print 'there has %s rows record' % count
+
+    result = cur.fetchone()
+    print result
+    print 'ID: %s info %s' % result
+
+    results = cur.fetchmany(5)
+    for r in results:
+        print
+
+    print '=='*10
+    cur.scroll(0,mode='absolute')
+
+    results = cur.fetchall()
+    for r in results:
+        print r[1]
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+except MySQLdb.Error,e:
+    print "Mysql Error %d: %s" % (e.args[0],e.args[1])
